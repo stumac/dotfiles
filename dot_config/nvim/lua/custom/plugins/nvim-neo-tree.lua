15e9ec53e339bd6e08af47a3f67f651241db2f76
stumac <stuart.g.macgregor@gmail.com>
2024-10-06 17:03:56 -0400

    initial commit

---
+++ dot_config/nvim/lua/custom/plugins/nvim-neo-tree.lua
@@ -0,0 +1,26 @@
+local plugin = {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    }
+  end,
+  keys = {
+    { '<leader>ft', '<cmd>Neotree toggle<cr>', desc = 'NeoTree toggle' },
+  },
+}
+
+return plugin
